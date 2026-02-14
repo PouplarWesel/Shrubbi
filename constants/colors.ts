286504cf313e9451776ba82f00cadb9b054664d1
PouplarWesel <94089618+PouplarWesel@users.noreply.github.com>
2026-02-13 22:49:14 -0800

    chore(theme): update app color palette

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,7 +1,13 @@
 export const COLORS = {
-  text: "#abd8bd",
-  background: "#000f0d",
-  primary: "#bff4fd",
-  secondary: "#a9d0c6",
-  accent: "#072900",
+  // Previous palette:
+  // text: "#abd8bd",
+  // background: "#000f0d",
+  // primary: "#bff4fd",
+  // secondary: "#a9d0c6",
+  // accent: "#072900",
+  text: "#E0E7E3",
+  background: "#0D1310",
+  primary: "#1A241F",
+  secondary: "#C3FFAD",
+  accent: "#88A096",
 } as const;
